2cf1a52a1cc12662c955a37ca975185f3d7dda35
grimz11 <andogreymark@gmail.com>
2020-01-17 17:53:35 +0800

    implemented testing using ject library

--- frontend/src/redux/actions/personActions.js
+++ frontend/src/redux/actions/personActions.js
@@ -11,7 +11,7 @@ const baseLiveURL = "https://morning-mountain-91801.herokuapp.com";
 const baseLocalURL = "http://localhost:5000";
 
 export const getAllPerson = () => async dispatch => {
-  const res = await axios.get(`${baseLocalURL}/api/users`);
+  const res = await axios.get(`${baseLiveURL}/api/users`);
   dispatch({
     type: FETCH_ALL_PERSON,
     payload: res.data
@@ -19,7 +19,7 @@ export const getAllPerson = () => async dispatch => {
 };
 
 export const deleteSinglePerson = ({ id }) => async dispatch => {
-  const res = await axios.delete(`${baseLocalURL}/api/users/${id}`);
+  const res = await axios.delete(`${baseLiveURL}/api/users/${id}`);
   dispatch({
     type: DELETE_SINGLE_PERSON,
     id: res.data.id
@@ -30,7 +30,7 @@ export const updateSinglePerson = payload => async dispatch => {
   const { id, firstname, lastname, birthday, address, hobbies } = payload;
   await axios({
     method: "PUT",
-    url: `${baseLocalURL}/api/users/${id}`,
+    url: `${baseLiveURL}/api/users/${id}`,
     headers: { "Content-Type": "application/json" },
     referrerPolicy: "no-referrer",
     data: {
@@ -48,7 +48,7 @@ export const updateSinglePerson = payload => async dispatch => {
 };
 
 export const getSinglePerson = ({ id }) => async dispatch => {
-  const res = await axios.get(`${baseLocalURL}/api/users/${id}`);
+  const res = await axios.get(`${baseLiveURL}/api/users/${id}`);
   console.log(res.data);
   dispatch({
     type: FETCH_SINGLE_PERSON,
