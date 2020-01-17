d2906bd39ea5e05828c43dea704ea9694dd34175
grimz11 <andogreymark@gmail.com>
2020-01-17 09:18:58 +0800

    edited the server js file

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
